a40de8235ad75f6614d59f18186e151544a0de92
Andrew Capp <acapp909@gmail.com>
2019-12-17 22:48:00 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
 class Pokemon
+  attr_accessor :name, :type, :db
+  attr_reader :id
+
+  def initialize(id:, name:, type:, db:)
+    @id, @name, @type, @db = id, name, type, db
+  end
+
+  def self.save(name, type, db)
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO pokemon(name, type) VALUES(?,?)
+      SQL
+      db[:conn].execute(sql, name, type)
+      self.id = db[:conn].execute("SELECT LAST INSERT ID()")[0][0]
+      self
+    end
+  end
+
+  def update
+
+  end
 end
